e4e7cee1983a858a9a33868270081825c4b63ec4
Nitesh Meshram <nitesh.meshram@v2solutions.com>
2015-02-26 16:33:21 +0530

    First Commit

---
+++ TravisExample/TravisExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TravisExample
+//
+//  Created by Nitesh Meshram on 26/02/15.
+//  Copyright (c) 2015 V2Solutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TravisExample/TravisExample/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TravisExample
+//
+//  Created by Nitesh Meshram on 26/02/15.
+//  Copyright (c) 2015 V2Solutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
